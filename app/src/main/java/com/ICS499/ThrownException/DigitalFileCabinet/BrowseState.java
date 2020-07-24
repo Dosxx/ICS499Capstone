8d466f8eb8fa2b430323ebbcd819440a85d4884c
Dosxx <dosakouete@gmail.com>
2020-07-24 15:26:39 -0500

    Ui CreateAccountActivity validate user input

--- app/src/main/java/com/ICS499/ThrownException/DigitalFileCabinet/BrowseState.java
+++ app/src/main/java/com/ICS499/ThrownException/DigitalFileCabinet/BrowseState.java
@@ -11,34 +11,35 @@ public class BrowseState implements DFCState {
     }
 
     @Override
-    public void createAccount() {/* This state is not responsible for this behavior */}
+    public DFCState createAccount() {return null;}
 
     @Override
-    public void deleteAccount() {/* This state is not responsible for this behavior */}
+    public DFCState deleteAccount() {return null;}
 
     @Override
-    public void login() {/* This state is not responsible for this behavior */}
+    public DFCState login() {return null;}
 
     @Override
-    public void logout() {/* This state is not responsible for this behavior */}
+    public DFCState logout() {return null;}
 
     @Override
-    public void openDoc() {/* This state is not responsible for this behavior */}
+    public DFCState openDoc() {return null;}
 
     @Override
-    public void saveDoc() {/* This state is not responsible for this behavior */}
+    public DFCState saveDoc() {return null;}
 
     @Override
-    public void createDoc() {/* This state is not responsible for this behavior */}
+    public DFCState createDoc() {return null;}
 
     @Override
-    public void importDoc() {/* This state is not responsible for this behavior */}
+    public DFCState importDoc() {return null;}
 
     @Override
-    public void browse() {
+    public DFCState browse() {
         // TODO implement browsing functionality here
+        return null;
     }
 
     @Override
-    public void deleteDoc() {/* This state is not responsible for this behavior */}
+    public DFCState deleteDoc() {return null;}
 }
