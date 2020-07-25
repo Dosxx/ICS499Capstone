ab2160fc5630613f1eb88d287f2ebbbb3108edc7
Dosxx <dosakouete@gmail.com>
2020-07-25 09:15:23 -0500

    adding camera features

--- app/src/main/java/com/ICS499/ThrownException/DigitalFileCabinet/EditDocumentActivity.java
+++ app/src/main/java/com/ICS499/ThrownException/DigitalFileCabinet/EditDocumentActivity.java
@@ -1,14 +1,41 @@
 package com.ICS499.ThrownException.DigitalFileCabinet;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditDocumentActivity extends AppCompatActivity {
-
+    private ImageView imageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_document);
+
+        imageView = findViewById(R.id.documentImageView);
+
+    }
+
+    public void takePicture(View view) {
+        Intent imageTakeIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(imageTakeIntent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(imageTakeIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
