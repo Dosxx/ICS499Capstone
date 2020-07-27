7ed3e3d19796fa390fe68c3197f0456948d111ff
Dosxx <dosakouete@gmail.com>
2020-07-27 16:45:59 -0500

    logout implemented

---
+++ app/src/main/java/com/ICS499/ThrownException/DigitalFileCabinet/Cryptographer.java
@@ -0,0 +1,26 @@
+package com.ICS499.ThrownException.DigitalFileCabinet;
+
+import android.app.Application;
+
+public class Cryptographer extends Application {
+    private String cipherText, clearText;
+
+
+    public String encrypt(Document document) {
+        return "cipherText";
+    }
+
+    public String decrypt(Document document) {
+        return "cipherText";
+    }
+
+    public String getCipherText() {
+        return cipherText;
+    }
+
+    public String getClearText() {
+        return clearText;
+    }
+
+
+}
